37cbc20817487c4a77419f32c43806c822168e33
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-07-14 14:08:57 +0300

    tasks done

--- js/task-2.js
+++ js/task-2.js
@@ -1,40 +1,34 @@
-function calcAverageCalories(days) {
-  const caloriesArray = [];
-  for (const day of days) {
-    caloriesArray.push(day.calories);
+class Storage {
+  #items;
+
+  constructor(produtcs) {
+    this.#items = produtcs;
   }
-  let sumCalories = 0;
-  for (const caloriesOfDay of caloriesArray) {
-    sumCalories += caloriesOfDay;
+
+  getItems() {
+    return this.#items;
   }
-  if (days.length === 0) {
-    return 0;
+  addItem(newItem) {
+    this.#items.push(newItem);
   }
-  return sumCalories / days.length;
+
+  removeItem(itemToRemove) {
+    this.#items.find((item, index) => {
+      if (item === itemToRemove) {
+        this.#items.splice(index, 1);
+      }
+    });
+  }
 }
 
-console.log(
-  calcAverageCalories([
-    { day: 'monday', calories: 3010 },
-    { day: 'tuesday', calories: 3200 },
-    { day: 'wednesday', calories: 3120 },
-    { day: 'thursday', calories: 2900 },
-    { day: 'friday', calories: 3450 },
-    { day: 'saturday', calories: 3280 },
-    { day: 'sunday', calories: 3300 },
-  ])
-); // 3180
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-console.log(
-  calcAverageCalories([
-    { day: 'monday', calories: 2040 },
-    { day: 'tuesday', calories: 2270 },
-    { day: 'wednesday', calories: 2420 },
-    { day: 'thursday', calories: 1900 },
-    { day: 'friday', calories: 2370 },
-    { day: 'saturday', calories: 2280 },
-    { day: 'sunday', calories: 2610 },
-  ])
-); // 2270
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
-console.log(calcAverageCalories([])); // 0
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
